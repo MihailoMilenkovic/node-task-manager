8009fba4b1e191ad65c1134e9c2146fdc5f3a988
MihailoMilenkovic <mihailo.milenkovic4@gmail.com>
2020-07-16 07:38:42 +0200

    init commit

---
+++ src/emails/account.js
@@ -0,0 +1,25 @@
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const sendWelcomeEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: "mihailo.milenkovic4@gmail.com",
+    subject: "Welcome to the note app",
+    text: `Hello ${name}. Thanks for using the application. Please give feedback about the app`,
+  });
+};
+
+const sendRemovalEmail = (email, name) => {
+  sgMail.send({
+    to: email,
+    from: "mihailo.milenkovic4@gmail.com",
+    subject: "Note app account removal",
+    text: `${name}, thank you for using the note app. You have successfully removed your account`,
+  });
+};
+
+module.exports = {
+  sendWelcomeEmail,
+  sendRemovalEmail,
+};
